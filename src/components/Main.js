575d83cad264aa9674e76ae3389713a9438bc05b
Kevin Wong <kevin.wong954@gmail.com>
2025-05-18 20:56:14 -0400

    update manifest config to v3, fix formatting

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,149 +1,143 @@
 /*global chrome*/
-import React, { useState, useEffect } from "react";
-import MusicCard from "./musicCard/MusicCards";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { YT_API_KEY } from "../util/secrets";
-import axios from "axios";
-import "../css/Main.css";
+import React, {useState, useEffect} from 'react'
+import MusicCard from './musicCard/MusicCards'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import YT_API_KEY from '../util/secrets'
+import axios from 'axios'
+import '../css/Main.css'
 
-const Main = ({ userAccessToken }) => {
-  const [musicRes, setMusicRes] = useState([]);
-  const [showQr, setShowQr] = useState(false);
-  const [userURI, setUserURI] = useState([]);
-  const [userID, setuserID] = useState("");
-  const [progress, setProgress] = useState("none");
-  const [noVideoDisplay, setNoVideoDisplay] = useState("none");
-  const [musictitle, setMusicTitle] = useState("");
+const Main = ({userAccessToken}) => {
+  const [musicRes, setMusicRes] = useState([])
+  const [showQr, setShowQr] = useState(false)
+  const [userURI, setUserURI] = useState([])
+  const [userID, setuserID] = useState('')
+  const [progress, setProgress] = useState('none')
+  const [noVideoDisplay, setNoVideoDisplay] = useState('none')
+  const [musictitle, setMusicTitle] = useState('')
 
   const getUrl = async () => {
-    return chrome.tabs.query(
-      { active: true, lastFocusedWindow: true },
-      async (tabs) => {
-        let url = tabs[0].url;
+    return chrome.tabs.query({active: true, lastFocusedWindow: true}, async (tabs) => {
+      let url = tabs[0].url
 
-        let id = url.split("v=")[1];
-        //checks if extra part to url
-        if (id.includes("&")) {
-          id = id.split("&")[0];
-        }
-
-        let title = await fetchYoutube(id);
-
-        chrome.storage.sync.get(["title"], function (result) {});
-
-        // let title = "Kendrick Lamar - HUMBLE";
-
-        await fetchData(title);
-        await setMusicTitle(title);
-        // use `url` here inside the callback because it's asynchronous!
+      let id = url.split('v=')[1]
+      //checks if extra part to url
+      if (id.includes('&')) {
+        id = id.split('&')[0]
       }
-    );
-  };
+
+      let title = await fetchYoutube(id)
+
+      chrome.storage.sync.get(['title'], function (result) {})
+
+      // let title = "Kendrick Lamar - HUMBLE";
+
+      await fetchData(title)
+      await setMusicTitle(title)
+      // use `url` here inside the callback because it's asynchronous!
+    })
+  }
 
   //gets user information
   const fetchUser = async () => {
     try {
       let res = await axios({
-        method: "get",
+        method: 'get',
         url: `https://api.spotify.com/v1/me/`,
         headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + userAccessToken,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + userAccessToken,
         },
-      });
+      })
       // if (!res.data.uri) chrome.storage.sync.set({ SK: "" }, function () {});
-      setUserURI(res.data.uri);
+      setUserURI(res.data.uri)
     } catch (error) {
-      console.log(error);
-      chrome.storage.sync.set({ SK: "" }, function () {});
+      console.log(error)
+      chrome.storage.sync.set({SK: ''}, function () {})
     }
-  };
+  }
 
   //get songs
 
   const fetchData = async (videoTitle) => {
-    setProgress("block");
+    setProgress('block')
     try {
       let res = await axios({
-        method: "get",
+        method: 'get',
         url: `https://api.spotify.com/v1/search/`,
         params: {
           q: videoTitle,
-          type: "track",
-          market: "US",
+          type: 'track',
+          market: 'US',
           limit: 10,
         },
         headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + userAccessToken,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + userAccessToken,
         },
-      });
+      })
 
       //stops progress
-      setProgress("none");
+      setProgress('none')
       //no results
       if (!musicRes.length) {
-        setNoVideoDisplay("block");
+        setNoVideoDisplay('block')
       }
       let sortedData = res.data.tracks.items.sort(function (a, b) {
-        return b.popularity - a.popularity;
-      });
-      setMusicRes(sortedData);
+        return b.popularity - a.popularity
+      })
+      setMusicRes(sortedData)
     } catch (error) {
-      console.log(error);
-      setProgress("none");
-      setNoVideoDisplay("block");
+      console.log(error)
+      setProgress('none')
+      setNoVideoDisplay('block')
     }
-  };
+  }
 
   //get youtube information for title
   const fetchYoutube = async (videoID) => {
     try {
-      let res = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search`,
-        {
-          params: {
-            part: "snippet",
-            maxResults: 8,
-            key: YT_API_KEY,
-            type: "video",
-            q: videoID,
-          },
-        }
-      );
-      let title = res.data.items[0].snippet.title;
-      if (title.includes("(")) {
-        title = title.split("(")[0];
+      let res = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+        params: {
+          part: 'snippet',
+          maxResults: 8,
+          key: YT_API_KEY,
+          type: 'video',
+          q: videoID,
+        },
+      })
+      let title = res.data.items[0].snippet.title
+      if (title.includes('(')) {
+        title = title.split('(')[0]
       }
 
-      return title;
+      return title
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const runExtension = async () => {
-    fetchUser();
-    getUrl();
-  };
+    fetchUser()
+    getUrl()
+  }
 
   useEffect(() => {
-    setNoVideoDisplay("none");
-    runExtension();
-  }, []);
+    setNoVideoDisplay('none')
+    runExtension()
+  }, [runExtension])
 
   //logout of spot
   const handleLogout = () => {
-    chrome.storage.sync.set({ SK: "" }, function () {});
-    window.close();
-  };
+    chrome.storage.sync.set({SK: ''}, function () {})
+    window.close()
+  }
   //redo Search
 
   const redoSearch = () => {
-    fetchData(musictitle);
-  };
+    fetchData(musictitle)
+  }
 
   return (
     <div className="mainComponent">
@@ -151,7 +145,7 @@ const Main = ({ userAccessToken }) => {
         <img
           id="closeBtn"
           onClick={() => {
-            window.close();
+            window.close()
           }}
           alt="close"
           src="https://static.thenounproject.com/png/73078-200.png"
@@ -161,16 +155,12 @@ const Main = ({ userAccessToken }) => {
           id="profileIcon"
           src="https://i.ibb.co/PhhB10N/profileimg.png"
           onClick={() => {
-            setShowQr(!showQr);
+            setShowQr(!showQr)
           }}
         />
         <h2 className="titleTag">YouTube to Spotify</h2>
         {showQr ? (
-          <img
-            id="qrImg"
-            alt="profileQr"
-            src={`https://api.qrserver.com/v1/create-qr-code/?data=${userURI}&amp.png`}
-          />
+          <img id="qrImg" alt="profileQr" src={`https://api.qrserver.com/v1/create-qr-code/?data=${userURI}&amp.png`} />
         ) : null}
         <button onClick={handleLogout}>Logout</button>
 
@@ -181,42 +171,22 @@ const Main = ({ userAccessToken }) => {
       </div>
       <div className="resultsList">
         <a href="https://open.spotify.com/" target="_blank">
-          <img
-            src={
-              "https://www.flaticon.com/svg/static/icons/svg/2111/2111627.svg"
-            }
-            alt="spotImg"
-            id="spotImg"
-          />
+          <img src={'https://www.flaticon.com/svg/static/icons/svg/2111/2111627.svg'} alt="spotImg" id="spotImg" />
         </a>
-        <p>search term:</p>{" "}
-        <input
-          value={musictitle}
-          onChange={(e) => setMusicTitle(e.currentTarget.value)}
-        />
+        <p>search term:</p> <input value={musictitle} onChange={(e) => setMusicTitle(e.currentTarget.value)} />
         <button onClick={redoSearch}>redo-search</button>
         {musicRes.length ? (
-          <MusicCard
-            result={musicRes}
-            userAccessToken={userAccessToken}
-            userURI={userURI}
-          />
+          <MusicCard result={musicRes} userAccessToken={userAccessToken} userURI={userURI} />
         ) : (
-          <p style={{ display: noVideoDisplay }}>
-            oops! no result try a different video or redo search
-          </p>
+          <p style={{display: noVideoDisplay}}>oops! no result try a different video or redo search</p>
         )}
-        <CircularProgress style={{ display: progress }} />
-        <a
-          id="githubLink"
-          href=" https://github.com/kwong0419/Music-Extension"
-          target="_blank"
-        >
+        <CircularProgress style={{display: progress}} />
+        <a id="githubLink" href=" https://github.com/kwong0419/Music-Extension" target="_blank">
           <h3 color="white">FAQ or Buy Developers Coffee 🙌 ☕</h3>
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
--- src/components/UserOauth.js
+++ src/components/UserOauth.js
@@ -1,63 +1,59 @@
-import React, { useState, useEffect } from "react";
-import Main from "./Main";
-import axios from "axios";
+import React, {useState, useEffect} from 'react'
+import Main from './Main'
+import axios from 'axios'
 
-import { makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { green } from "@material-ui/core/colors";
-import { red } from "@material-ui/core/colors";
-import Button from "@material-ui/core/Button";
+import {makeStyles} from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import {green} from '@material-ui/core/colors'
+import {red} from '@material-ui/core/colors'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
   },
   wrapper: {
     margin: theme.spacing(1),
-    position: "relative",
+    position: 'relative',
   },
   buttonSuccess: {
     backgroundColor: green[500],
-    "&:hover": {
+    '&:hover': {
       backgroundColor: green[700],
     },
   },
 
   buttonProgress: {
     color: green[500],
-    position: "absolute",
-    top: "50%",
-    left: "50%",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
     marginTop: -12,
     marginLeft: -12,
   },
-}));
+}))
 /* global chrome */
 
-const CLIENT_ID = encodeURIComponent("7c15aba07d5a4cf394a8b745c6df7bac");
-const RESPONSE_TYPE = encodeURIComponent("token");
+const CLIENT_ID = encodeURIComponent('68f4136733804885ad7b21f568e6da7d')
+const RESPONSE_TYPE = encodeURIComponent('token')
 //local redirect--Below--
-const REDIRECT_URI = encodeURIComponent(
-  "https://enlcncnogcnflpiioonanamloaggnemm.chromiumapp.org/"
-);
+const REDIRECT_URI = encodeURIComponent('https://ahcigmedajhdjdoigkkjhmlkjpajoafj.chromiumapp.org/')
 //live redirect --Below--
 // const REDIRECT_URI = encodeURIComponent(
 //   "https://dfjdbnkpphnaohmlnmjhjkjeheaaojon.chromiumapp.org/"
 // );
 const SCOPE = encodeURIComponent(
-  "ugc-image-upload playlist-read-collaborative playlist-modify-private playlist-modify-public playlist-read-private user-read-playback-position user-read-recently-played user-top-read user-modify-playback-state user-read-currently-playing user-read-playback-state user-read-private user-read-email user-library-modify user-library-read user-follow-modify user-follow-read streaming app-remote-control"
-);
-const SHOW_DIALOG = encodeURIComponent("true");
-let STATE = "";
-let ACCESS_TOKEN = "";
+  'ugc-image-upload playlist-read-collaborative playlist-modify-private playlist-modify-public playlist-read-private user-read-playback-position user-read-recently-played user-top-read user-modify-playback-state user-read-currently-playing user-read-playback-state user-read-private user-read-email user-library-modify user-library-read user-follow-modify user-follow-read streaming app-remote-control',
+)
+const SHOW_DIALOG = encodeURIComponent('true')
+let STATE = ''
+let ACCESS_TOKEN = ''
 
-let user_signed_in = false;
+let user_signed_in = false
 
 const create_spotify_endpoint = () => {
-  STATE = encodeURIComponent(
-    "meet" + Math.random().toString(36).substring(2, 15)
-  );
+  STATE = encodeURIComponent('meet' + Math.random().toString(36).substring(2, 15))
 
   let oauth2_url = `https://accounts.spotify.com/authorize
 ?client_id=${CLIENT_ID}
@@ -66,18 +62,16 @@ const create_spotify_endpoint = () => {
 &state=${STATE}
 &scope=${SCOPE}
 &show_dialog=${SHOW_DIALOG}
-`;
+`
 
-  // console.log(oauth2_url);
+  return oauth2_url
+}
 
-  return oauth2_url;
-};
-
-const UserOauthDisplay = ({ userAccessToken, setUserAccessToken }) => {
-  const classes = useStyles();
-  const [loading, setLoading] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const timer = React.useRef();
+const UserOauthDisplay = ({userAccessToken, setUserAccessToken}) => {
+  const classes = useStyles()
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const timer = React.useRef()
 
   const userOauthFlow = () => {
     chrome.identity.launchWebAuthFlow(
@@ -90,48 +84,44 @@ const UserOauthDisplay = ({ userAccessToken, setUserAccessToken }) => {
         if (chrome.runtime.lastError) {
           //   sendResponse({ message: "fail" });
 
-          console.log(chrome.runtime.lastError);
+          console.log(chrome.runtime.lastError)
         } else {
-          if (redirect_url.includes("callback?error=access_denied")) {
+          if (redirect_url.includes('callback?error=access_denied')) {
             // sendResponse({ message: "fail" });
           } else {
-            handleKey(redirect_url);
+            handleKey(redirect_url)
           }
         }
-      }
-    );
-    return ACCESS_TOKEN;
-  };
+      },
+    )
+    return ACCESS_TOKEN
+  }
+
   const handleKey = (redirect_url) => {
-    ACCESS_TOKEN = redirect_url.substring(
-      redirect_url.indexOf("access_token=") + 13
-    );
+    ACCESS_TOKEN = redirect_url.substring(redirect_url.indexOf('access_token=') + 13)
 
-    ACCESS_TOKEN = ACCESS_TOKEN.substring(0, ACCESS_TOKEN.indexOf("&"));
-    let state = redirect_url.substring(redirect_url.indexOf("state=") + 6);
-    chrome.storage.sync.set({ SK: ACCESS_TOKEN }, function () {
-      // console.log("Value is set to " + ACCESS_TOKEN);
-    });
+    ACCESS_TOKEN = ACCESS_TOKEN.substring(0, ACCESS_TOKEN.indexOf('&'))
+    let state = redirect_url.substring(redirect_url.indexOf('state=') + 6)
+    chrome.storage.sync.set({SK: ACCESS_TOKEN}, function () {})
     if (state === STATE) {
-      console.log("SUCCESS");
-      user_signed_in = true;
+      console.log('SUCCESS')
+      user_signed_in = true
       setTimeout(() => {
-        ACCESS_TOKEN = "";
-        user_signed_in = false;
-        chrome.storage.sync.set({ SK: "" }, function () {});
-      }, 100000);
+        ACCESS_TOKEN = ''
+        user_signed_in = false
+        chrome.storage.sync.set({SK: ''}, function () {})
+      }, 100000)
 
-      //   7200000
-
-      chrome.browserAction.setPopup({ popup: "./index.html" }, () => {});
+      chrome.browserAction.setPopup({popup: './index.html'}, () => {})
     }
-  };
-  const handleClick = async () => {
-    userOauthFlow();
+  }
 
-    setSuccess(false);
-    setLoading(true);
-  };
+  const handleClick = async () => {
+    userOauthFlow()
+
+    setSuccess(false)
+    setLoading(true)
+  }
 
   return (
     <>
@@ -156,19 +146,14 @@ const UserOauthDisplay = ({ userAccessToken, setUserAccessToken }) => {
               onClick={handleClick}
             >
               sign in
-              {loading && (
-                <CircularProgress
-                  size={20}
-                  className={classes.buttonProgress}
-                />
-              )}
+              {loading && <CircularProgress size={20} className={classes.buttonProgress} />}
             </Button>
             {loading ? <p>use pop-up to sign in</p> : null}
           </div>
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default UserOauthDisplay;
+export default UserOauthDisplay
